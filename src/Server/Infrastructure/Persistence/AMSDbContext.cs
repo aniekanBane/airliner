0f187b609995f1022651af77d1f0fc7bbdec1b68
aniekan <77311836+aniekanBane@users.noreply.github.com>
2023-11-29 03:22:32 +0100

    feat: add dbcontext

---
+++ src/Server/Infrastructure/Persistence/AMSDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities.AMS.Aircrafts;
+using Domain.Entities.AMS.AircraftTypes;
+using Persistence.Configurations;
+
+using Npgsql;
+
+namespace Persistence;
+
+public sealed class AMSDbContext(DbContextOptions<AMSDbContext> options) : DbContext(options)
+{
+    public DbSet<Aircraft> Aircrafts => Set<Aircraft>();
+    public DbSet<AircraftType> AircraftTypes => Set<AircraftType>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new AircraftConfiguration());
+        modelBuilder.ApplyConfiguration(new AircraftTypeConfiguration());
+
+        modelBuilder.HasPostgresEnum<CabinClass>();
+        modelBuilder.HasPostgresEnum<AircraftStatus>();
+    }
+
+    public static NpgsqlDataSource AMSSource(string conn)
+    {
+        var sourceBuiler = new NpgsqlDataSourceBuilder(conn);
+
+        sourceBuiler.MapEnum<AircraftStatus>();
+        sourceBuiler.MapEnum<CabinClass>();
+
+        return sourceBuiler.Build();
+    }
+}
---
+++ src/Server/Infrastructure/Persistence/ARSDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities.AMS.Aircrafts;
+using Domain.Entities.ARS.DestinationAggregate;
+using Domain.Entities.ARS.FlightAggregate;
+using Domain.Entities.ARS.FlightRoute;
+using Persistence.Configurations;
+
+using Npgsql;
+
+namespace Persistence;
+
+internal sealed class ARSDbContext(DbContextOptions<ARSDbContext> options) : DbContext(options)
+{
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<Airport> Airports => Set<Airport>();
+    public DbSet<Flight> Flights => Set<Flight>();
+    public DbSet<FlightRoute> FlightRoutes => Set<FlightRoute>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new CityConfiguration());
+        modelBuilder.ApplyConfiguration(new AirportConfiguration());
+        modelBuilder.ApplyConfiguration(new FlightConfiguration());
+        modelBuilder.ApplyConfiguration(new FlightRouteConfiguration());
+
+        modelBuilder.HasPostgresEnum<RouteStatus>();
+        modelBuilder.HasPostgresEnum<FlightStatus>();
+        modelBuilder.HasPostgresEnum<FlightType>();
+        modelBuilder.HasPostgresEnum<CabinClass>();
+    }
+
+    public static NpgsqlDataSource ARSSource(string conn)
+    {
+        var sourceBuiler = new NpgsqlDataSourceBuilder(conn);
+
+        sourceBuiler.UseNodaTime();
+        sourceBuiler.MapEnum<RouteStatus>();
+        sourceBuiler.MapEnum<FlightStatus>();
+        sourceBuiler.MapEnum<FlightType>();
+        sourceBuiler.MapEnum<CabinClass>();
+
+        return sourceBuiler.Build();
+    }
+}
