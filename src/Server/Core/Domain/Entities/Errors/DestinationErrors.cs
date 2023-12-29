8635deeaf92abe4883e76ed444e3875e8c913456
aniekan <77311836+aniekanBane@users.noreply.github.com>
2023-12-29 01:30:27 +0100

    refactor: config

---
+++ src/Server/Core/Domain/Entities/Errors/DestinationErrors.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Primitives.Results;
+
+namespace Domain.Entities.Errors;
+
+public static partial class DomainErrors 
+{
+    public static class Airport
+    {
+        public static readonly Error Exists = new(
+            "Airport.Exists", 
+            "Destination already exists!");
+
+        public static readonly Error NotFound = new(
+            "Airport.NotFound", 
+            "Destination does not exist!");
+
+        public static readonly Error TerminalExists = new(
+            "Terminal.Exists", 
+            "Aiport terminal already exists!");
+    }
+
+    public static class City
+    {
+        public static readonly Error Exists = new(
+            "City.Exists", 
+            "Destination already exists!");
+            
+        public static readonly Error NotFound = new(
+            "City.NotFound", 
+            "Destination does not exist!");
+    }
+}
